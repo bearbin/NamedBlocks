50720e36e051e1e08904445b5e053b7c1420d748
Alexander Harkness <bearbin@gmail.com>
2013-01-08 20:49:22 +0000

    Added preliminary blocks list.

---
+++ blocks.lua
@@ -0,0 +1,146 @@
+-- Copyright (c) 2012 Alexander Harkness
+
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the
+-- "Software"), to deal in the Software without restriction, including
+-- without limitation the rights to use, copy, modify, merge, publish,
+-- distribute, sublicense, and/or sell copies of the Software, and to
+-- permit persons to whom the Software is furnished to do so, subject to
+-- the following conditions:
+
+-- The above copyright notice and this permission notice shall be included
+-- in all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+-- OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+-- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+-- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+-- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+-- Block names start here
+
+BLOCKS = {}
+
+blocks[1] = {"Air"}
+blocks[2] = {"Stone"}
+blocks[3] = {"Grass"}
+blocks[4] = {"Dirt"}
+blocks[5] = {"Cobblestone"}
+blocks[6] = {"Oak Wood Planks", "Spruce Wood Planks", "Birch Wood Planks", "Jungle Wood Planks"}
+blocks[7] = {"Oak Sapling", "Spruce Sapling", "Birch Sapling", "Jungle Sapling"}
+blocks[8] = {"Bedrock"}
+blocks[9] = {"Water"}
+blocks[10] = {"Stationary Water"}
+blocks[11] = {"Lava"}
+blocks[12] = {"Stationary Lava"}
+blocks[13] = {"Sand"}
+blocks[14] = {"Gravel"}
+blocks[15] = {"Gold Ore"}
+blocks[16] = {"Iron Ore"}
+blocks[17] = {"Coal Ore"}
+blocks[18] = {"Oak Wood", "Spruce Wood", "Birch Wood", "Jungle Wood"}
+blocks[19] = {"Oak Leaves", "Spruce Leaves", "Birch Leaves", "Jungle Leaves"}
+blocks[20] = {"Sponge"}
+blocks[21] = {"Glass"}
+blocks[22] = {"Lapis Lazuli Ore"}
+blocks[23] = {"Lapis Lazuli Block"}
+blocks[24] = {"Dispenser"}
+blocks[25] = {"Sandstone"}
+blocks[26] = {"Note Block"}
+blocks[27] = {"Bed Block"}
+blocks[28] = {"Powered Rail"}
+blocks[29] = {"Detector Rail"}
+blocks[30] = {"Sticky Piston"}
+blocks[31] = {"Web"}
+blocks[32] = {"Dead Shrub", "Tall Grass", "Fern"}
+blocks[33] = {"Dead Shrub"}
+blocks[34] = {"Piston"}
+blocks[35] = {"Piston Head"}
+blocks[36] = {}
+blocks[36][1] = "White Wool"
+blocks[36][2] = "Orange Wool"
+blocks[36][3] = "Magenta Wool"
+blocks[36][4] = "Light Blue Wool"
+blocks[36][5] = "Yellow Wool"
+blocks[36][6] = "Light Green Wool"
+blocks[36][7] = "Pink Wool"
+blocks[36][8] = "Gray Wool"
+blocks[36][9] = "Light Gray Wool"
+blocks[36][10] = "Cyan Wool"
+blocks[36][11] = "Purple Wool"
+blocks[36][12] = "Blue Wool"
+blocks[36][13] = "Brown Wool"
+blocks[36][14] = "Dark Green Wool"
+blocks[36][15] = "Red Wool"
+blocks[36][16] = "Black Wool"
+blocks[38] = {"Dandelion"}
+blocks[39] = {"Rose"}
+blocks[40] = {"Brown Mushroom"}
+blocks[41] = {"Red Mushroom"}
+blocks[42] = {"Gold Block"}
+blocks[43] = {"Iron Block"}
+blocks[44] = {"Double Stone Slab", "Double Sandstone Slab", "Double Wooden Slab", "Double Cobblestone Slab", "Double Brick Slab", "Double Stone Brick Slab"}
+blocks[45] = {"Stone Slab", "Sandstone Slab", "Wooden Slab", "Cobblestone Slab", "Brick Slab", "Stone Brick Slab"}
+blocks[46] = {"Brick"}
+blocks[47] = {"TNT"}
+blocks[48] = {"Bookshelf"}
+blocks[49] = {"Mossy Cobblestone"}
+blocks[50] = {"Obsidian"}
+blocks[51] = {"Torch"}
+blocks[52] = {"Fire"}
+blocks[53] = {"Monster Spawner"}
+blocks[54] = {"Wooden Stairs"}
+blocks[55] = {"Chest"}
+blocks[56] = {"Redstone Wire"}
+blocks[57] = {"Diamond Ore"}
+blocks[58] = {"Diamond Block"}
+blocks[59] = {"Workbench"}
+blocks[60] = {"Wheat Crops"}
+blocks[61] = {"Soil"}
+blocks[62] = {"Furnace"}
+blocks[63] = {"Burning Furnace"}
+blocks[64] = {"Standing Sign"}
+blocks[65] = {"Wooden Door Block"}
+blocks[66] = {"Ladder"}
+blocks[67] = {"Rails"}
+blocks[68] = {"Cobblestone Stairs"}
+blocks[69] = {"Wall Sign"}
+blocks[70] = {"Lever"}
+blocks[71] = {"Stone Pressure Plate"}
+blocks[72] = {"Iron Door Block"}
+blocks[73] = {"Wooden Pressure Plate"}
+blocks[74] = {"Redstone Ore"}
+blocks[75] = {"Glowing Redstone Ore"}
+blocks[76] = {"Redstone Torch (Off)"}
+blocks[77] = {"Redstone Torch (On)"}
+blocks[78] = {"Stone Button"}
+blocks[79] = {"Snow"}
+blocks[80] = {"Ice"}
+blocks[81] = {"Snow Block"}
+blocks[82] = {"Cactus"}
+blocks[83] = {"Clay"}
+blocks[84] = {"Sugar Cane"}
+blocks[85] = {"Jukebox"}
+blocks[86] = {"Fence"}
+blocks[87] = {"Pumpkin"}
+blocks[88] = {"Netherrack"}
+blocks[89] = {"Soul Sand"}
+blocks[90] = {"Glowstone"}
+blocks[91] = {"Portal Block"}
+blocks[92] = {"Jack-O-Lantern"}
+blocks[93] = {"Cake Block"}
+blocks[94] = {"Redstone Repeater (Off)"}
+blocks[95] = {"Redstone Repeater (On)"}
+blocks[96] = {"Locked Chest"}
+blocks[97] = {"Trapdoor"}
+blocks[98] = {"Silverfish Stone", "Silverfish Cobblestone", "Silverfish Stone Brick"}
+blocks[99] = {"Stone Brick", "Mossy Stone Brick", "Cracked Stone Brick"}
+blocks[100] = {"Red Mushroom Cap"}
+blocks[101] = {"Brown Mushroom Cap"}
+blocks[102] = {"Iron Bars"}
+blocks[103] = {"Glass Pane"}
+blocks[104] = {"Melon Block"}
+
